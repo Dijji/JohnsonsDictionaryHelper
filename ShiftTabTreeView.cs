0b967843ea365cdbe3a10c83943cb7b8f71c866f
Dijji_cp <27084934+Dijji@users.noreply.github.com>
2014-03-23 10:27:35 +0000

    Add Shift-Tab support to tree Update version to 0.9.3

---
+++ ShiftTabTreeView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Johnson
+{
+    public class ShiftTabTreeView : TreeView
+    {
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if ((Keyboard.Modifiers & ModifierKeys.Shift) != 0 && e.Key == Key.Tab)
+                return;
+
+            base.OnKeyDown(e);
+        }
+    }
+}
